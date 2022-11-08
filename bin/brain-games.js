41dd46e1babe483e959c726fd7ff856930140d27
Vladislav Kovalenko <papoakr@gmail.com>
2022-11-08 15:06:20 +0600

    fix code

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,7 +2,6 @@
 import { getName } from '../src/cli.js';
 import { readAnswer } from './brain-even.js';
 
-console.log('Welcom to the Brain Games!');
 getName();
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 readAnswer();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
+console.log('Welcom to the Brain Games!');
 export const name = readlineSync.question('May i have you name? ');
 
-export function getName () {
+export function getName() {
   console.log(`Hello, ${name}`);
 }
+ 
