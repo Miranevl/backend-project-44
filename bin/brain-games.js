6e06bb2142b750aeffe8c2266838c68700ceba99
Vladislav Kovalenko <papoakr@gmail.com>
2022-11-09 12:57:26 +0600

    moving file in games directory

--- bin/brain-games.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { getName } from '../src/cli.js';
-import { readAnswer } from './brain-even.js';
-
-getName();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-readAnswer();
