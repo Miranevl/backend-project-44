614ab4746e4184aa054e1ade27353eb85d527eed
Vladislav Kovalenko <papoakr@gmail.com>
2022-11-11 11:50:13 +0600

    add new games

---
+++ bin/games/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { gcdGames } from '../../src/index.js';
+
+gcdGames();
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import readlineSync from 'readline-sync';
+import _ from 'lodash';
 import { getName, name } from './cli.js';
 import { readAnswer } from '../bin/brain-even.js';
 
 export function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+  const Data = new Date();
+  const Seconds = Data.getSeconds();
+  const result = Math.trunc(Seconds % max) / 2;
+  if (result === 0) {
+    return 1;
+  }
+  return Math.trunc(Seconds % max);
 }
 
 function getRandomOperation() {
@@ -18,6 +25,27 @@ function Calculate(operator, num1, num2) {
     case '*': return num1 * num2; break;
   }
 }
+function getMaxOfArray(numArray) {
+  return Math.max.apply(null, numArray);
+}
+
+function findGcd(num1, num2) {
+  let i = 1;
+  const resultOne = [];
+  const resultTwo = [];
+  while (i <= num1 && i <= num2) {
+    if (num1 % i === 0) {
+      resultOne.push(i);
+    }
+    if (num2 % i === 0) {
+      resultTwo.push(i);
+    }
+    i += 1;
+  }
+  const result = _.intersection(resultOne, resultTwo);
+  const resultM = getMaxOfArray(result);
+  return resultM;
+}
 
 export const brainGames = () => {
   getName();
@@ -30,7 +58,7 @@ export const calcGames = () => {
   console.log('What is the result of the expression?');
   let roundWin = 0;
   while (roundWin < 3) {
-    const saveRandomOne = getRandomInt(10);
+    const saveRandomOne = getRandomInt(25);
     const saveRandomTwo = getRandomInt(10);
     const saveOperand = getRandomOperation();
     const saveResult = Calculate(saveOperand, saveRandomOne, saveRandomTwo);
@@ -41,6 +69,28 @@ export const calcGames = () => {
       roundWin += 1;
     } else {
       console.log(`${answerUser} is wrong answer ;(. Correct answer was ${saveResult}.`);
+      console.log(`Let's try again' ${name}!`);
+      return 0;
+    }
+  }
+  return console.log(`Congratulations,${name}!`);
+};
+
+export const gcdGames = () => {
+  getName();
+  console.log('Find the greatest common divisor of given numbers.');
+  let roundWin = 0;
+  while (roundWin < 3) {
+    const saveRandomOne = getRandomInt(25);
+    const saveRandomTwo = getRandomInt(10);
+    console.log(`Question: ${saveRandomOne} ${saveRandomTwo}`);
+    const savegcd = findGcd(saveRandomOne, saveRandomTwo);
+    const answerUser = readlineSync.question('Your answer: ');
+    if (savegcd === Number(answerUser)) {
+      console.log('Correct!');
+      roundWin += 1;
+    } else {
+      console.log(`${answerUser} is wrong answer ;(. Correct answer was ${savegcd}.`);
       console.log(`Let's try again' ${name}!`);
       return 0;
     }
