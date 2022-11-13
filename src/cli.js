a8424a6fc179b67024f1de277f06bd7939ef6851
Vladislav Kovalenko <papoakr@gmail.com>
2022-11-13 20:25:53 +0600

    fix word

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const getName = () => {
-  console.log('Welcom to the Brain Games!');
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May i have you name? ');
   console.log(`Hello, ${name}`);
 };
